7bb14578c68055e23ac84e6c039b644f3e066931
Narnian <58570301+Plotnik12@users.noreply.github.com>
2020-10-17 17:47:50 +0300

    Лабораторная работа номер 1

--- task2.pl
+++ task2.pl
@@ -1,6 +1,14 @@
-% Task 2: Relational Data
+:- include('two.pl').
 
-% The line below imports the data
-:- ['one.pl'].
+len([],0).
+len([H|T],N):- len(T,A), N is A + 1. 
 
-group(X,L) :- findall(Z,student(X,Z),L).
+sum([],0).
+sum([H|T],N):- sum(T,A), N is A + H.
+
+
+sr_ball(T,N):- bagof(X,A^B^grade(A,B,T,X),L), len(L,F), sum(L,K), N is K/F.
+
+ne_sdalgr(G,T):- bagof(2,A^B^grade(G,A,B,2),L), len(L,T).
+
+ne_sdalpr(X,T):-  bagof(2,A^B^grade(A,B,X,2),L), len(L,T).
